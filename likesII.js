b948e35bd3a9c7ddb35c033bd679d636af6df75e
Gitelez1 <nesti.elezi@icloud.com>
2024-04-02 16:48:35 +0200

    first commit

---
+++ likesII.js
@@ -0,0 +1,32 @@
+// BBUTTON 1
+var count = 9
+
+function addLikeButton1() {
+    var countElement = document.querySelector("#count1")
+    count++;
+    countElement.innerText = count + "Like(s)";
+    console.log(count);
+}
+
+// BBUTTON 2
+var count = 12
+
+function addLikeButton2() {
+    var countElement = document.querySelector("#count2")
+    count++;
+    countElement.innerText = count + "Like(s)";
+    console.log(count);
+}
+
+// // BBUTTON 3
+var count = 9
+
+console.log(countElement);
+
+function addLikeButton3() {
+    var countElement = document.querySelector("#count3")
+    count++;
+    countElement.innerText = count + "Like(s)";
+    console.log(count);
+}
+
